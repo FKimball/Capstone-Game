711e33620c25ed4967e698478eee22ea66436e4b
FKimball <156947100+FKimball@users.noreply.github.com>
2025-05-06 23:15:22 -0400

    More level development

---
+++ Frankie_Tests/Camera Tests/chest_gold.gd
@@ -0,0 +1,9 @@
+extends MeshInstance3D
+
+func _ready():
+	$Panel.visible=false
+	
+func _on_area_3d_body_entered(body) :
+	if body.name == "Player":
+		print("Player touched the area!")  # Debug message
+		$Panel.visible = true
