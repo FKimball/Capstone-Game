325e88bfe40062191e3b951b53369e061cf661a2
FKimball <156947100+FKimball@users.noreply.github.com>
2025-05-08 19:10:46 -0400

    Misc. Fixes

--- Frankie_Tests/Camera Tests/chest_gold.gd
+++ Frankie_Tests/Camera Tests/chest_gold.gd
@@ -7,5 +7,6 @@ func _on_area_3d_body_entered(body) :
 	if body.name == "Player":
 		print("Player touched the area!")  # Debug message
 		$Panel.visible = true
-		$"../../AudioStreamPlayer3D".stop()
-		$victorymusic.play()
+		$"../../Player/backgroundmusic".stop()
+		Victorymusic.play()
+		
--- Frankie_Tests/Camera Tests/how_to_play.gd
+++ Frankie_Tests/Camera Tests/how_to_play.gd
@@ -6,4 +6,5 @@ func _ready():
 	play_button.pressed.connect(_on_play_pressed)
 
 func _on_play_pressed():
+	Audiobutton.play()
 	get_tree().change_scene_to_file("res://main_menu.tscn")
