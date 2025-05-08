c2758bf9d65b370a096fbcc22d29e1a2a18a88a3
mhoque16 <mhoque16@fordham.edu>
2025-05-08 03:43:45 -0400

    cover page

---
+++ Frankie_Tests/Camera Tests/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready():
+	# Connect buttons by their node names
+	$MarginContainer/VBoxContainer/Button.pressed.connect(_on_start_game_pressed)
+	$MarginContainer/VBoxContainer/Button3.pressed.connect(_on_how_to_play_pressed)
+	$MarginContainer/VBoxContainer/Button2.pressed.connect(_on_quit_pressed)
+
+func _on_start_game_pressed():
+	get_tree().change_scene_to_file("res://scenes/Lab4.tscn")  # replace with your game scene path
+
+func _on_how_to_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/how_to_play.tscn")  # replace with your tutorial scene path
+
+func _on_quit_pressed():
+	get_tree().quit()
