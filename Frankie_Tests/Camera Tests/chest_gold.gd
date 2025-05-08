75c8cccfde05247f5cff77da2022945ebf08375f
mhoque16 <mhoque16@fordham.edu>
2025-05-08 16:03:26 -0400

    more changes

--- Frankie_Tests/Camera Tests/chest_gold.gd
+++ Frankie_Tests/Camera Tests/chest_gold.gd
@@ -7,4 +7,5 @@ func _on_area_3d_body_entered(body) :
 	if body.name == "Player":
 		print("Player touched the area!")  # Debug message
 		$Panel.visible = true
+		$"../../AudioStreamPlayer3D".stop()
 		$victorymusic.play()
--- Frankie_Tests/Camera Tests/main_menu.gd
+++ Frankie_Tests/Camera Tests/main_menu.gd
@@ -4,24 +4,21 @@ extends Control
 @onready var how_to_play_button = $MarginContainer/VBoxContainer/HowToPlayButton
 @onready var quit_button = $MarginContainer/VBoxContainer/QuitButton
 
-func _ready():
+func _ready(): 
 	start_button.pressed.connect(_on_start_pressed)
 	how_to_play_button.pressed.connect(_on_how_pressed)
 	quit_button.pressed.connect(_on_quit_pressed)
 
-
 func _on_start_pressed():
+	Audiobutton.play()
 	get_tree().change_scene_to_file("res://Lab4.tscn")  # Make sure this path is correct
-
-func _on_how_pressed():
-	get_tree().change_scene_to_file("res://how_to_play.tscn")  # Replace with your real scene path
-
 func _on_start_game_pressed():
+	Audiobutton.play()
 	get_tree().change_scene_to_file("res://Lab4.tscn")  # replace with your game scene path
-
-func _on_how_to_play_pressed():
-	get_tree().change_scene_to_file("res://howto.tscn")  # replace with your tutorial scene path
-
-
+func _on_how_pressed():
+	Audiobutton.play()
+	get_tree().change_scene_to_file("res://how_to_play.tscn")  # Replace with your real scene path
 func _on_quit_pressed():
+	Audiobutton.play()
 	get_tree().quit()
+	
