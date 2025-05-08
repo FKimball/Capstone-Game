418fb6f0be49780c184adca4e3cfffe7b325be54
FKimball <156947100+FKimball@users.noreply.github.com>
2025-05-08 13:54:26 -0400

    Added music

--- Frankie_Tests/Camera Tests/main_menu.gd
+++ Frankie_Tests/Camera Tests/main_menu.gd
@@ -10,7 +10,7 @@ func _on_start_game_pressed():
 	get_tree().change_scene_to_file("res://Lab4.tscn")  # replace with your game scene path
 
 func _on_how_to_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/how_to_play.tscn")  # replace with your tutorial scene path
+	get_tree().change_scene_to_file("res://howto.tscn")  # replace with your tutorial scene path
 
 func _on_quit_pressed():
 	get_tree().quit()
